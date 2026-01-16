a5fe58aef8a9947b88b358950d8c6c27dda948d9
Anton <70934030+friendlyantz@users.noreply.github.com>
2026-01-17 07:32:19 +1100

    LEET217: add more memory and space efficient solutions

--- leetcode/217_contains_duplicate/solution.rb
+++ leetcode/217_contains_duplicate/solution.rb
@@ -23,14 +23,51 @@
 #     -109 <= nums[i] <= 109
 
 def contains_duplicate(nums)
+  # hash_set(nums)
+  sorting_approach(nums)
+  # naive_builtin_method_solution(nums)
+
+end
+
+# BEST SOLUTION: Hash Set Approach
+# Time: O(n), Space: O(n)
+# Use a hash set to track seen numbers. If we encounter a number already in the set, return true.
+# This is optimal because hash lookups are O(1) average case.
+def hash_set(nums)
+  set = Set.new
+  nums.each do |e|
+    # check  if it is exists O(n)
+    return true if set.include?(e)
+    # if not seen -> add to resuts array or circuit break
+    set.add e
+  end
+  false
+end
+
+# ALTERNATIVE: Sorting Approach: Slower, but more space efficient but slower.
+# Time: O(n log n), Space: O(1) or O(n) depending on sort implementation
+# Sort the array, then check adjacent elements.
+def sorting_approach(nums)
+  nums.sort!
+  (1...nums.length).each do |i|
+    pointer  = nums[i]
+    previous_value = nums[i - 1]
+    return true if pointer.eql? previous_value
+  end
+  false
+end
+
+# SIMPLE BUT LESS EFFICIENT: Built-in methods
+# Uses Ruby's uniq which also uses hash table internally, but creates full new array
+def naive_builtin_method_solution
   # nums.tally.any? { |_k, v| v > 1 }
   nums.uniq != nums
 end
 
-RSpec.describe 'Solution' do
+RSpec.describe "Solution" do
   let(:input_one) { [1, 1, 1, 3, 3, 4, 3, 2, 4, 2] }
   let(:input_two) { [1, 2, 3, 4] }
-  it 'return true for double up values and false if all unique' do
+  it "return true for double up values and false if all unique" do
     expect(contains_duplicate(input_one)).to eq true
     expect(contains_duplicate(input_two)).to eq false
   end
